0a98b80e8c1e9774edf4b3d859824630d74c279c
korakij ngaobenjakul <korakij-wmw@hotmail.com>
2023-09-11 15:12:59 +0700

    update

--- MNG.APIs/MNG.UI/Production/frmPouring.Designer.cs
+++ MNG.APIs/MNG.UI/Production/frmPouring.Designer.cs
@@ -36,17 +36,19 @@
             // pnPouring
             // 
             this.pnPouring.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pnPouring.Location = new System.Drawing.Point(938, 0);
+            this.pnPouring.Location = new System.Drawing.Point(1251, 0);
+            this.pnPouring.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnPouring.Name = "pnPouring";
-            this.pnPouring.Size = new System.Drawing.Size(580, 720);
+            this.pnPouring.Size = new System.Drawing.Size(773, 865);
             this.pnPouring.TabIndex = 226;
             // 
             // pnKanban
             // 
             this.pnKanban.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pnKanban.Location = new System.Drawing.Point(251, 0);
+            this.pnKanban.Location = new System.Drawing.Point(335, 0);
+            this.pnKanban.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnKanban.Name = "pnKanban";
-            this.pnKanban.Size = new System.Drawing.Size(687, 720);
+            this.pnKanban.Size = new System.Drawing.Size(916, 865);
             this.pnKanban.TabIndex = 228;
             // 
             // pnLotNo
@@ -54,21 +56,23 @@
             this.pnLotNo.BackColor = System.Drawing.Color.White;
             this.pnLotNo.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnLotNo.Location = new System.Drawing.Point(0, 0);
+            this.pnLotNo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnLotNo.Name = "pnLotNo";
-            this.pnLotNo.Size = new System.Drawing.Size(251, 720);
+            this.pnLotNo.Size = new System.Drawing.Size(335, 865);
             this.pnLotNo.TabIndex = 226;
             // 
             // frmPouring
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1720, 720);
+            this.ClientSize = new System.Drawing.Size(1942, 886);
             this.Controls.Add(this.pnPouring);
             this.Controls.Add(this.pnKanban);
             this.Controls.Add(this.pnLotNo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmPouring";
             this.Text = "MNG - SPECTROMETERING";
             this.Load += new System.EventHandler(this.frmPouring_Load);
